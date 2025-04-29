470a70e0b18e6869cf2c2ffb6a54f09c3f936bdc
Dinesh Taral <dineshtaral@Dineshs-MacBook-Air.local>
2025-04-29 21:55:39 +0530

    recoil added

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import NavBar from "./components/navbar";
+import { BrowserRouter } from 'react-router-dom';
+import { RecoilRoot } from 'recoil';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <NavBar/>
+    <RecoilRoot>
+
+    <BrowserRouter>
+
     <App/>
+    </BrowserRouter>
+    </RecoilRoot>
+
   </StrictMode>,
 )
